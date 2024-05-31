9a60d876527e17f783232ee00b4958bc36d20fe6
f-PLT <fplt.softwaredeveloper@gmail.com>
2024-05-31 10:34:08 -0400

    Set pyogrio as default I/O engin for geopandas

--- geospatial_tools/__init__.py
+++ geospatial_tools/__init__.py
@@ -1,7 +1,12 @@
 from pathlib import Path
 
+import geopandas as gpd
+
 APP_ROOT = Path(__file__).resolve().parent
 PROJECT_ROOT = APP_ROOT.parent
 CONFIG = PROJECT_ROOT / "config"
 DATA_DIR = PROJECT_ROOT / "data"
 SCRIPTS_DIR = PROJECT_ROOT / "scripts"
+
+# Set the default I/O engine
+gpd.options.io_engine = "pyogrio"
