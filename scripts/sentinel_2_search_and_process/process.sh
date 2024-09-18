a94712d316e412767fb8b65f409951832d10f4b0
f-PLT <fplt.softwaredeveloper@gmail.com>
2024-09-18 18:35:11 -0400

    Update scripts

--- scripts/sentinel_2_search_and_process/process.sh
+++ scripts/sentinel_2_search_and_process/process.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=process_s2_products
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --mem=32Gb
 #SBATCH --cpus-per-task=4
 
 module load miniconda/3
 conda activate geospatial-tools
 
-CURRENT_DIR="$(dirname "$0")"
-cd "${CURRENT_DIR}" || exit 1
-source ../config.sh
+source config.sh
 
 if [ "$1" == "" ]; then
   DEFAULT_ARGS=("--help")
--- scripts/sentinel_2_search_and_process/search.sh
+++ scripts/sentinel_2_search_and_process/search.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=search_s2_products
-#SBATCH --time=01:00:00
+#SBATCH --time=01:30:00
 #SBATCH --mem=32Gb
 #SBATCH --cpus-per-task=4
 
 module load miniconda/3
 conda activate geospatial-tools
 
-CURRENT_DIR="$(dirname "$0")"
-cd "${CURRENT_DIR}" || exit 1
-source ../config.sh
+source config.sh
 
-if [ "$1" == "" ]; then
-  DEFAULT_ARGS=("--help")
-else
-  DEFAULT_ARGS=("${@:1}")
-fi
+DEFAULT_ARGS=("${@:1}")
 
 python3 "${PROJECT_DIR}"/scripts/sentinel_2_search_and_process/product_search.py "${DEFAULT_ARGS[@]}"
