b7898c0585d3f941a042617a2da0252158c935c7
f-PLT <fplt.softwaredeveloper@gmail.com>
2024-09-18 10:47:25 -0400

    Add arg management to scripts

--- scripts/sentinel_2_search_and_process/search.sh
+++ scripts/sentinel_2_search_and_process/search.sh
@@ -4,11 +4,17 @@
 #SBATCH --mem=32Gb
 #SBATCH --cpus-per-task=4
 
-#module load miniconda/3
-#conda activate geospatial-tools
+module load miniconda/3
+conda activate geospatial-tools
 
 CURRENT_DIR="$(dirname "$0")"
-cd "${CURRENT_DIR}" || exit
+cd "${CURRENT_DIR}" || exit 1
 source ../config.sh
 
-python3 "${PROJECT_DIR}"/scripts/sentinel_2_search_and_process/product_search.py --help
+if [ "$1" == "" ]; then
+  DEFAULT_ARGS=("--help")
+else
+  DEFAULT_ARGS=("${@:1}")
+fi
+
+python3 "${PROJECT_DIR}"/scripts/sentinel_2_search_and_process/product_search.py "${DEFAULT_ARGS[@]}"
